e3ce9bd15f07181b65530687df4b32b79dd70f29
zarean <zarean@inside.sahab.ir>
2020-01-25 19:13:10 +0330

    First Commit

---
+++ Assets/Scripts/PathGizmos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGizmos : MonoBehaviour
+{
+    void OnStart() {
+    }
+
+    void OnDrawGizmos()
+    {
+        Transform[] path = gameObject.transform.GetComponentsInChildren<Transform>();
+        iTween.DrawPath(path);
+        Gizmos.color = Color.blue;
+        foreach(Transform t in path) 
+        {
+            Gizmos.DrawSphere(t.position, 0.1f);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/PathWalker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathWalker : MonoBehaviour
+{
+    public float speed = 5;
+    public Transform path;
+    public Transform[] pathNodes;
+    float progress = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    pathNodes = path.GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress += speed * 0.01f * Time.deltaTime;
+        iTween.PutOnPath(gameObject, pathNodes, progress);
+    }
+}
